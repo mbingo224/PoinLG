9e0d886a362cda234be88c1041ae0f760ce44b12
zjin <mbingo@163.com>
2022-11-21 12:05:21 +0800

    修改模型在kitti测试数据 集上的测试效果和新增一个py测试用例观察补全前后点云图中点的数量变化

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,7 @@
 
 set -x
 GPUS=$1
+# $@表示命令行所有参数，${@:2}表示从命令行的第二个参数开始截取到最后一个参数
 PY_ARGS=${@:2}
 
 CUDA_VISIBLE_DEVICES=${GPUS} python main.py --test ${PY_ARGS}
---
+++ test_net_model_py/test.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+import os 
+
+pred_data = torch.from_numpy(np.load(os.path.join("/home/zjin/Desktop/PoinTr/experiments/PoinTr/KITTI_models/test_example/vis_result", "frame_0_car_0_000", 'input.npy'))).unsqueeze(0).cuda()
+print(type(pred_data))
+print(pred_data.shape)
+print(pred_data)
