8dbbb6f2d4110516ab716ff117040027956d9c90
zjin <mbingo@163.com>
2023-01-05 16:57:57 +0800

    完成命令行参数解析argparse模块的学习(utils/parser.py),理解了训练和测试时的实验数据(模型文件 损失日志)的生成目录,可使用tensorboard去进行可视化和checkpoint恢复训练

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,2 +1,2 @@
-from .runner import run_net
+from .runner import run_net # 对于tools包 __init__.py会先将runner模块下的run_net函数导入，包外面再想导入这些内容时，就可以用from tools import 函数名来导入，main.py中导入即是这样使用的
 from .runner import test_net
--- utils/parser.py
+++ utils/parser.py
@@ -2,12 +2,14 @@ import os
 import argparse
 from pathlib import Path
 
+# 解析命令行参数，并根据命令行参数进行异常处理，并创建放置训练生成的模型文件的目录，设置日志目录，将参数返回
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--config', 
         type = str, 
         help = 'yaml config file')
+    # choices=['none', 'pytorch']表示参数可以接受的值的范围
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch'],
@@ -20,7 +22,7 @@ def get_args():
     parser.add_argument(
         '--deterministic',
         action='store_true',
-        help='whether to set deterministic options for CUDNN backend.')      
+        help='whether to set deterministic options for CUDNN backend.') # action='store_true'当这一选项存在时，为 args.deterministic 赋值为 True没有指定时则隐含地赋值为 False。      
     # bn
     parser.add_argument(
         '--sync_bn', 
@@ -29,7 +31,8 @@ def get_args():
         help='whether to use sync bn')
     # some args
     parser.add_argument('--exp_name', type = str, default='default', help = 'experiment name')
-    parser.add_argument('--start_ckpts', type = str, default=None, help = 'reload used ckpt path')
+    # 当想要仅微调某个已训练的模型，就可使用--start_ckpts参数来指定该模型的权重文件（包含了 model_state_dict 和 optimizer_state_dict ）以在其上继续训练
+    parser.add_argument('--start_ckpts', type = str, default=None, help = 'reload used ckpt path') 
     parser.add_argument('--ckpts', type = str, default=None, help = 'test used ckpt path')
     parser.add_argument('--val_freq', type = int, default=1, help = 'test freq')
     parser.add_argument(
@@ -56,11 +59,12 @@ def get_args():
     if args.resume and args.start_ckpts is not None:
         raise ValueError(
             '--resume and --start_ckpts cannot be both activate')
-
+    # test时应加载一个ckpts权重文件来进行评估
     if args.test and args.ckpts is None:
         raise ValueError(
             'ckpts shouldnt be None while test mode')
 
+    # 在操作系统中定义的环境变量，全部保存在os.environ这个变量中(字典的形式)，如果LOCAL_RANK并不在环境变量中，向其中添加LOCAL_RANK变量
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
 
@@ -68,6 +72,8 @@ def get_args():
         args.exp_name = 'test_' + args.exp_name
     if args.mode is not None:
         args.exp_name = args.exp_name + '_' +args.mode
+    # Path(args.config).stem将会返回当前路径中的主文件名，这里的当前路径即指args.config参数所表示的路径，例如对于args.config = "./cfgs/PCN_models/PoinTr.yaml"，
+    # Path(args.config).stem将会取得 PoinTr，Path(args.config).parent.stem将会取得PCN_models，因此再拼接上args.exp_name = "example" 可得到 args.experiment_path = "./experiments/PoinTr/PCN_models/example"
     args.experiment_path = os.path.join('./experiments', Path(args.config).stem, Path(args.config).parent.stem, args.exp_name)
     args.tfboard_path = os.path.join('./experiments', Path(args.config).stem, Path(args.config).parent.stem,'TFBoard' ,args.exp_name)
     args.log_name = Path(args.config).stem
