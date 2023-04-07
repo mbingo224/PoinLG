e118e49257e0b9ebe8e4f831cf69abcb1d7debff
zjin <mbingo@163.com>
2023-04-07 20:10:58 +0800

    Fun：修正模型在PCN测试集上的表现的脚本中参数路径

---
+++ scripts/test_model_command_sh/test_pcn.sh
@@ -0,0 +1,4 @@
+bash ./scripts/test.sh 0 \
+    --ckpts ./experiments/PoinTr/PCN_models/Experiments_11_bs_44_num_query=256/ckpt-best.pth \
+    --config ./cfgs/PCN_models/PoinTr.yaml \
+    --exp_name Experiments_11_bs_44_num_query=256
