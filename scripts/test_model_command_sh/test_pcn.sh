16b952f9f23fa9cfa4fe1e3af8ee26cf4890df49
zjin <mbingo@163.com>
2023-04-07 16:33:02 +0800

    Fun：新增一个test_pcn.sh测试模型在PCN数据集上的性能

---
+++ scripts/test_model_command_sh/test_pcn.sh
@@ -0,0 +1,4 @@
+bash ./scripts/test.sh 0 \
+    --ckpts experiments/PoinTr/PCN_models/Experiments_8__bs_36/ckpt-best.pth \
+    --config ./cfgs/PCN_models/PoinTr.yaml \
+    --exp_name Experiments_8__bs_36
