f9590507ae6a55afc06c13ff34f31b67ef744010
zjin <mbingo@163.com>
2023-04-12 15:11:55 +0800

    实验8-4：训练基于多通道分辨率 Transformer 的点云不全模型在SN-2数据集 添加了训练集对应的脚本

--- scripts/test_command_ShapeNet.sh
+++ scripts/test_command_ShapeNet.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0,1 bash ./scripts/dist_train.sh 2 13232 \
-    --config ./cfgs/ShapeNet34_models/PoinTr.yaml \
+    --config ./cfgs/ShapeNet55_models/PoinTr.yaml \
     --exp_name Experiments_8_bs_48
