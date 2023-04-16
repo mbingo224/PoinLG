471668894c2628bfc41bcd15e1049932132a197c
zjin <mbingo@163.com>
2023-04-16 17:11:27 +0800

    Fun:添加训练和测试相应的脚本，注意测试时需要分别设置不同难度的测试模式

---
+++ scripts/command_sh/test_command_ShapeNet-55.sh
@@ -0,0 +1,5 @@
+bash ./scripts/test.sh 0 \
+    --ckpts ./experiments/PoinTr/ShapeNet55_models/Experiments_8_bs_48/ckpt-best.pth \
+    --config ./cfgs/ShapeNet55_models/PoinTr.yaml \
+    --mode easy \ # 有 easy, moderate, hard 三种模式，分别对应不同的测试集，最后求平均来作为最佳结果
+    --exp_name Experiments_8_bs_48
