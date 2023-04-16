1c3866fa1978dbf99df1555c2a4f50f92188a3f4
zjin <mbingo@163.com>
2023-04-16 21:17:10 +0800

    Fun：修改测试实验的模式

--- scripts/command_sh/test_command_ShapeNet-55.sh
+++ scripts/command_sh/test_command_ShapeNet-55.sh
@@ -4,8 +4,8 @@ bash ./scripts/test.sh 0 \
     --mode easy \ # 有 easy, median, hard 三种模式，分别对应不同的测试集，最后求平均来作为最佳结果
     --exp_name Experiments_8_bs_48
 
-    bash ./scripts/test.sh 1 \
+bash ./scripts/test.sh 1 \
     --ckpts ./experiments/PoinTr/ShapeNet55_models/Experiments_8_bs_48/ckpt-best.pth \
     --config ./cfgs/ShapeNet55_models/PoinTr.yaml \
-    --mode median \
+    --mode hard \
     --exp_name Experiments_8_bs_48
