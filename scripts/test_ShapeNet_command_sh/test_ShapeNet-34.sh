5c0a7fae61dfc8f64cd1eda71029fede48b101f0
zjin <mbingo@163.com>
2023-04-16 16:54:10 +0800

    Fun:测试 ShapeNet-34 数据集的CD-L2和F-Score指标的脚本命令添加

---
+++ scripts/test_ShapeNet_command_sh/test_ShapeNet-34.sh
@@ -0,0 +1,5 @@
+bash ./scripts/test.sh 0 \
+    --ckpts ./experiments/PoinTr/ShapeNet34_models/Experiments_8_bs_48/ckpt-best.pth \
+    --config ./cfgs/ShapeNet34_models/PoinTr.yaml \
+    --mode easy \
+    --exp_name Experiments_8_bs_48
