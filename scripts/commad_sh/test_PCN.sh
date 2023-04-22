dbb9e7379466880b194e138db00cb115e1e16485
zjin <mbingo@163.com>
2023-04-22 12:49:46 +0800

    Fun：实验5模型添加测试PCN数据集的脚本 并进行测试实验

---
+++ scripts/commad_sh/test_PCN.sh
@@ -0,0 +1,4 @@
+bash ./scripts/test.sh 0 \
+    --ckpts ./experiments/PoinTr/PCN_models/Experiments_5_EdgeConvResFeat_bs_46/ckpt-best.pth \
+    --config ./cfgs/PCN_models/PoinTr.yaml \
+    --exp_name Experiments_5_EdgeConvResFeat_bs_46
