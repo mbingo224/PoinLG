296773a8a23eab417165c6f005c2fdb8e5a09bb6
zjin <mbingo@163.com>
2023-04-22 15:21:46 +0800

    重要Fun：解决了KITTI_metric.py中计算FD和MMD没有打印日志，不方便追溯计算结果，添加了FD和MMD的计算日志；同时KITTI数据集可视化需要同时生成txt和npy文件才能正确运行KITTI_metric.py

--- scripts/test_model_command_sh/test_KITTI.sh
+++ scripts/test_model_command_sh/test_KITTI.sh
@@ -1,8 +1,8 @@
 bash ./scripts/test.sh 0 \
-    --ckpts ./experiments/PoinTr/KITTI_models/Experiments_8_bs_38/ckpt-best.pth \
+    --ckpts ./experiments/PoinTr/KITTI_models/Finetune_Experiments_8_bs_38/ckpt-best.pth \
     --config ./cfgs/KITTI_models/PoinTr.yaml \
-    --exp_name Experiments_8_bs_38
+    --exp_name Finetune_Experiments_8_bs_38
 
 # KITTI 评估时，需要将测试集的生成的npy点云数据转换为图片，然后再执行下面的程序去进行评估
 CUDA_VISIBLE_DEVICES=0 python KITTI_metric.py \
-    --vis ./experiments/PoinTr/KITTI_models/test_Experiments_8_bs_38/vis_result # 这里的路径是上一步实验生成的测试目录下的可视化输出路径vis_result
+    --vis ./experiments/PoinTr/KITTI_models/test_Finetune_Experiments_8_bs_38/vis_result # 这里的路径是上一步实验生成的测试目录下的可视化输出路径vis_result
