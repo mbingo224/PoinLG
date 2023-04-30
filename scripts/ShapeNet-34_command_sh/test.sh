b926d5d6b5bbf7d84af49858a14301623ba0e274
zjin <mbingo@163.com>
2023-04-30 17:03:51 +0800

    测试实验：将实验5的模型进行ShapeNet-34进行三个难道等级评估

---
+++ scripts/ShapeNet-34_command_sh/test.sh
@@ -0,0 +1,5 @@
+bash ./scripts/test.sh 0 \
+    --ckpts ./experiments/PoinTr/ShapeNet34_models/Experiments_5_bs_64/ckpt-best.pth \
+    --config ./cfgs/ShapeNet34_models/PoinTr.yaml \
+    --mode hard \
+    --exp_name Experiments_5_bs_64
