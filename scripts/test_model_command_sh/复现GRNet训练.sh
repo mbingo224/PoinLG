cabff51f394e8d6c36a9a5f61f74fa0591cb79cb
zjin <mbingo@163.com>
2023-05-01 15:30:29 +0800

    复现GRNet实验：从实验11的ckpt-last.pth开始微调，观察是否可以达到CDL1=8.83，F-Score=0.708

---
+++ scripts/test_model_command_sh/复现GRNet训练.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0,1 bash ./scripts/dist_train.sh 2 13232 \
+    --config ./cfgs/PCN_models/PoinTr.yaml \
+    --exp_name 复现GRNet训练
+    --start_ckpts ./experiments/PoinTr/PCN_models/Experiments_11_bs_44_num_query=256/ckpt-last.pth
