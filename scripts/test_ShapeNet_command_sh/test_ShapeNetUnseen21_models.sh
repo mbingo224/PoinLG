1d43cf81ff77b41e7209fb9dcbe9dd274b330632
zjin <mbingo@163.com>
2023-05-04 19:51:17 +0800

    可视化评估实验：实验8模型在未见21类的easy模式下的可视化评估

--- scripts/test_ShapeNet_command_sh/test_ShapeNetUnseen21_models.sh
+++ scripts/test_ShapeNet_command_sh/test_ShapeNetUnseen21_models.sh
@@ -1,5 +1,5 @@
-bash ./scripts/test.sh 0 \
+bash ./scripts/test.sh 1 \
     --ckpts ./experiments/PoinTr/ShapeNet34_models/Experiments_8_bs_48/ckpt-best.pth \
     --config ./cfgs/ShapeNetUnseen21_models/PoinTr.yaml \
-    --mode median \
+    --mode easy \
     --exp_name Unseen21_Experiments_8_bs_48
