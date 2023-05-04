3d347a9acea705b805b01a00ab8f82feb8135762
zjin <mbingo@163.com>
2023-05-04 19:19:27 +0800

    评估实验：实验5模型在ShapeNet_unseen21上评估实验，并在easy模式下可视化一些类别

--- scripts/ShapeNet-34_command_sh/test.sh
+++ scripts/ShapeNet-34_command_sh/test.sh
@@ -1,5 +1,5 @@
 bash ./scripts/test.sh 0 \
     --ckpts ./experiments/PoinTr/ShapeNet34_models/Experiments_5_bs_64/ckpt-best.pth \
-    --config ./cfgs/ShapeNet34_models/PoinTr.yaml \
-    --mode hard \
-    --exp_name Experiments_5_bs_64
+    --config ./cfgs/ShapeNetUnseen21_models/PoinTr.yaml \
+    --mode easy \
+    --exp_name Experiments_5_bs_64_best
