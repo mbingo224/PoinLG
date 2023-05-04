114a1904c3bdfbf0acd0f7b5deeea6ee2fae1d57
zjin <mbingo@163.com>
2023-05-04 20:03:49 +0800

    可视化评估实验：PoinTr在ShapeNet的21个未见类别可视化评估 easy模式下

---
+++ scripts/test_ShapeNet_Unseen21_sh/test_ShapeNet_Unseen21.sh
@@ -0,0 +1,5 @@
+bash ./scripts/test.sh 1 \
+    --ckpts ./pretrained/pointr_training_from_scratch_c34_best.pth \
+    --config ./cfgs/ShapeNetUnseen21_models/PoinTr.yaml \
+    --mode easy \
+    --exp_name PoinTr
