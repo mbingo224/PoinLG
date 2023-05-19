8f9448ffd389cb8a5210b485be7999bcbcf22a70
zjin <mbingo@163.com>
2023-05-19 14:57:56 +0800

    实验：再次评估模型的flops和吞吐量

--- scripts/command_sh/计算FLOPS和吞吐量命令.sh
+++ scripts/command_sh/计算FLOPS和吞吐量命令.sh
@@ -1,4 +1,4 @@
 bash ./scripts/test.sh 0 \
-    --ckpts /media/zjin/Data_ZJ/experiments/GRNet/PCN_models/GRNet_Experiments_8_bs_4/ckpt-best.pth \
-    --config ./cfgs/PCN_models/GRNet.yaml \
-    --exp_name GRNet_计算Flops和吞吐量
+    --ckpts ./experiments/PoinTr/PCN_models/Experiments_8__bs_36/ckpt-best.pth \
+    --config ./cfgs/PCN_models/PoinTr.yaml \
+    --exp_name Exp_8_计算Flops和吞吐量
