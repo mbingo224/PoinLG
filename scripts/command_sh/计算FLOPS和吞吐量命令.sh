6972024723264794bb6e9b2be3ee61d3f604a0f7
zjin <mbingo@163.com>
2023-05-19 14:56:17 +0800

    Fun：完善计算模型的FLOPS和吞吐量的程序runner.py

---
+++ scripts/command_sh/计算FLOPS和吞吐量命令.sh
@@ -0,0 +1,4 @@
+bash ./scripts/test.sh 0 \
+    --ckpts /media/zjin/Data_ZJ/experiments/GRNet/PCN_models/GRNet_Experiments_8_bs_4/ckpt-best.pth \
+    --config ./cfgs/PCN_models/GRNet.yaml \
+    --exp_name GRNet_计算Flops和吞吐量
